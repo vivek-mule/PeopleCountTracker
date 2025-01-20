15047471fb8e79afae1d2816c68bcb5803faf096
Vivek Mule <136962159+vivek-mule@users.noreply.github.com>
2025-01-20 20:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,8 +129,9 @@ def handle_stream_frame(data):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT"))
     print(f"Starting server on port {port}")
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host="0.0.0.0", port=port)
+
     
     
